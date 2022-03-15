c62bc0e2d13740246c6dbce869b42ae53ca1e668
blazej-bryla <brykuski@gmail.com>
2022-03-16 00:29:32 +0100

    fix build error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import All from "./pages/all";
-import New from "./pages/new";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<All />} />
-      <Route path="/new" element={<New />} />
     </Routes>
   );
 }
